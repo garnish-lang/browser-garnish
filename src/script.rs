e2cc3c6122291a2009479bca2f7fe4fa58ca0342
Chad Collins <chadrcollins91@gmail.com>
2024-02-27 12:33:45 -0500

    implemented compile

--- src/script.rs
+++ src/script.rs
@@ -1,11 +1,16 @@
-use wasm_bindgen::prelude::wasm_bindgen;
+use garnish_lang::compiler::build::build_with_data;
+use garnish_lang::compiler::lex::{lex, Lexer, LexerToken};
+use garnish_lang::compiler::parse::parse;
 use garnish_lang::simple::SimpleGarnishData;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 struct GarnishScript {
     name: String,
     text: String,
+    source_tokens: Vec<LexerToken>,
     data: SimpleGarnishData,
+    error: Option<String>,
 }
 
 #[wasm_bindgen]
@@ -15,7 +20,9 @@ impl GarnishScript {
         GarnishScript {
             name,
             text,
-            data: SimpleGarnishData::new_custom(),
+            source_tokens: vec![],
+            data: SimpleGarnishData::new(),
+            error: None,
         }
     }
 
@@ -33,6 +40,49 @@ impl GarnishScript {
 
     pub fn set_text(&mut self, text: String) {
         self.text = text;
+    }
+
+    pub fn compile(&mut self) {
+        self.source_tokens = match lex(&self.text) {
+            Ok(tokens) => tokens,
+            Err(e) => {
+                self.error = Some(e.to_string());
+                return;
+            }
+        };
+
+        let parse_result = match parse(&self.source_tokens) {
+            Err(e) => {
+                self.error = Some(e.to_string());
+                return;
+            }
+            Ok(result) => result,
+        };
+
+        self.data = SimpleGarnishData::new();
+
+        match build_with_data(
+            parse_result.get_root(),
+            parse_result.get_nodes().clone(),
+            &mut self.data,
+        ) {
+            Err(e) => {
+                self.error = Some(e.to_string());
+                return;
+            }
+            Ok(_) => {}
+        }
+    }
+}
+
+// for methods that won't be exposed to JS
+impl GarnishScript {
+    pub fn get_source_tokens(&self) -> &Vec<LexerToken> {
+        &self.source_tokens
+    }
+
+    pub fn get_data(&self) -> &SimpleGarnishData {
+        &self.data
     }
 }
 
@@ -61,5 +111,14 @@ mod tests {
         script.set_text("10 + 10".to_string());
 
         assert_eq!(script.get_text(), "10 + 10".to_string());
+    }
+
+    #[test]
+    fn compile() {
+        let mut script = GarnishScript::new("test_one".to_string(), "5 + 5".to_string());
+        script.compile();
+
+        assert_eq!(script.get_source_tokens().len(), 5);
+        assert_eq!(script.get_data().get_data().len(), 4);
     }
 }
